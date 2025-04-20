acaa5113ed52dce3c7e95cbf6819b2a87237ecb9
johyeonjun <rhlehfndvkd7557@daum.net>
2025-04-20 16:23:52 +0900

    BOX, SKU에 대한 SQL 및 초기 설정 추가

--- src/main/resources/db/insert_data.sql
+++ src/main/resources/db/insert_data.sql
@@ -1,16 +1,16 @@
 TRUNCATE public.sku;
 
-INSERT INTO sku (sku_id, name, width, height, length, weight, shape)
-VALUES (1, 'A SKU', 3, 3, 1, 1, 'BOX'),
-       (2, 'B SKU', 3, 1, 1, 1, 'BOX'),
-       (3, 'C SKU', 3, 1, 1, 1, 'BOX'),
-       (4, 'D SKU', 3, 1, 1, 1, 'BOX'),
-       (5, 'E SKU', 1, 1, 3, 1, 'BOX'),
-       (6, 'F SKU', 1, 3, 1, 1, 'BOX'),
-       (7, 'G SKU', 3, 1, 1, 1, 'BOX');
+INSERT INTO sku (sku_id, name, width, height, length, weight, shape, created_at, updated_at)
+VALUES (1, 'A SKU', 3, 3, 1, 1, 'BOX', current_timestamp, current_timestamp),
+       (2, 'B SKU', 3, 1, 1, 1, 'BOX', current_timestamp, current_timestamp),
+       (3, 'C SKU', 3, 1, 1, 1, 'BOX', current_timestamp, current_timestamp),
+       (4, 'D SKU', 3, 1, 1, 1, 'BOX', current_timestamp, current_timestamp),
+       (5, 'E SKU', 1, 1, 3, 1, 'BOX', current_timestamp, current_timestamp),
+       (6, 'F SKU', 1, 3, 1, 1, 'BOX', current_timestamp, current_timestamp),
+       (7, 'G SKU', 3, 1, 1, 1, 'BOX', current_timestamp, current_timestamp);
 
 TRUNCATE box;
 
-INSERT INTO box (box_id, name, width, height, length, weight, max_weight)
-VALUES (1, 'Q BOX', 3, 3, 3, 1, 10),
-       (2, 'W BOX', 3, 3, 3, 1, 10);
+INSERT INTO box (box_id, name, width, height, length, weight, max_weight, created_at, updated_at)
+VALUES (1, 'Q BOX', 3, 3, 3, 1, 10, current_timestamp, current_timestamp),
+       (2, 'W BOX', 3, 3, 3, 1, 10, current_timestamp, current_timestamp);
