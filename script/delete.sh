b8f9796785869bbf4565d1f7fd9c03269ee72fff
johyeonjun <rhlehfndvkd7557@daum.net>
2025-04-22 12:50:45 +0900

    쿠버네티스로 배포할 수 있도록 관련 설정 파일 및 스크립트 추가

--- script/delete.sh
+++ script/delete.sh
@@ -1,6 +1,7 @@
 cd ..
-kubectl delete -f kubernetes/configmap.yaml
-kubectl delete -f kubernetes/postgres.yaml
-kubectl delete -f kubernetes/deployment.yaml
-kubectl delete -f kubernetes/service.yaml
-kubectl delete -f kubernetes/secret.yaml
+kubectl delete -f base/configmap.yaml
+kubectl delete -f base/postgres.yaml
+kubectl delete -f base/deployment.yaml
+kubectl delete -f base/service.yaml
+kubectl delete -f base/secret.yaml
+kubectl delete -f base/box-recommend-app.yaml
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,8 @@
 cd ..
 kubectl create namespace box-recommend
-kubectl apply -f kubernetes/configmap.yaml
-kubectl apply -f kubernetes/postgres.yaml
-kubectl apply -f kubernetes/deployment.yaml
-kubectl apply -f kubernetes/secret.yaml # 순서 중요(1)
-kubectl apply -f kubernetes/service.yaml # 순서 중요(2)
-
+kubectl apply -f base/configmap.yaml
+kubectl apply -f base/postgres.yaml
+kubectl apply -f base/deployment.yaml
+kubectl apply -f base/secret.yaml # 순서 중요(1)
+kubectl apply -f base/service.yaml # 순서 중요(2)
+kubectl apply -f base/box-recommend-app.yaml
