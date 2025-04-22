4c2bec236bef96d020bf12f9e1a87656c8656245
johyeonjun <rhlehfndvkd7557@daum.net>
2025-04-22 14:32:26 +0900

    쿠버네티스로 배포할 수 있도록 관련 설정 파일 및 스크립트 추가

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,5 @@ cd ..
 kubectl apply -k base/
 
 # node port 를 로컬에서 사용할 수 있도록 세팅
-kubectl port-forward svc/box-recommend 30080:80 &
-kubectl port-forward svc/postgres 5432:5432 &
+kubectl port-forward svc/box-recommend 30080:80
+kubectl port-forward svc/postgres 5432:5432
