5488667e31dc20ec9d01b58a7ef9724d2c137fd6
johyeonjun <rhlehfndvkd7557@daum.net>
2025-04-22 13:00:03 +0900

    쿠버네티스로 배포할 수 있도록 관련 설정 파일 및 스크립트 추가

--- script/delete.sh
+++ script/delete.sh
@@ -1,7 +1,2 @@
 cd ..
-kubectl delete -f base/configmap.yaml
-kubectl delete -f base/postgres.yaml
-kubectl delete -f base/deployment.yaml
-kubectl delete -f base/service.yaml
-kubectl delete -f base/secret.yaml
-kubectl delete -f base/box-recommend-app.yaml
+kubectl delete ../base/
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,2 @@
 cd ..
-kubectl create namespace box-recommend
-kubectl apply -f base/configmap.yaml
-kubectl apply -f base/postgres.yaml
-kubectl apply -f base/deployment.yaml
-kubectl apply -f base/secret.yaml # 순서 중요(1)
-kubectl apply -f base/service.yaml # 순서 중요(2)
-kubectl apply -f base/box-recommend-app.yaml
+kubectl apply -k base/
