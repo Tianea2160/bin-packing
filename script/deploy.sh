e092723b27791fee9984cbdbe8bcaa6a065c54a4
johyeonjun <rhlehfndvkd7557@daum.net>
2025-04-23 12:56:42 +0900

    헬스체크 로직 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,3 +3,5 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl apply -k base/
+# argocd 비밀번호 조회하기
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
