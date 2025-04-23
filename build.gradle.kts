48ae45380fcbc8d7cec9976b9762a98212bd8c77
johyeonjun <rhlehfndvkd7557@daum.net>
2025-04-23 13:40:18 +0900

    grafana, prometheus 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
