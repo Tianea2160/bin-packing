c1af73a28bb6c774d337e2434804ff8db84a5935
johyeonjun <rhlehfndvkd7557@daum.net>
2025-05-03 16:40:35 +0900

    주석 제거 및 생성 일시 버그 수정

--- src/main/kotlin/org/tianea/boxrecommend/domain/recommend/result/document/BinPackRecommendResult.kt
+++ src/main/kotlin/org/tianea/boxrecommend/domain/recommend/result/document/BinPackRecommendResult.kt
@@ -1,6 +1,7 @@
 package org.tianea.boxrecommend.domain.recommend.result.document
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.elasticsearch.annotations.DateFormat
 import org.springframework.data.elasticsearch.annotations.Document
 import org.springframework.data.elasticsearch.annotations.Field
 import org.springframework.data.elasticsearch.annotations.FieldType
@@ -11,37 +12,37 @@ import java.time.LocalDateTime
 data class BinPackRecommendResult(
     @Id
     val id: String? = null,
-    
+
     @Field(type = FieldType.Long)
     val solutionId: Long,
-    
+
     @Field(type = FieldType.Long)
     val recommendedBinId: Long,
-    
+
     @Field(type = FieldType.Text)
     val status: String,
-    
+
     @Field(type = FieldType.Long)
     val score: Long?,
-    
+
     @Field(type = FieldType.Text)
     val scoreDescription: String,
-    
+
     @Field(type = FieldType.Nested)
     val scoreCoordinates: List<ScoreCoordinate>,
-    
-    @Field(type = FieldType.Date)
+
+    @Field(type = FieldType.Date, format = [DateFormat.basic_date_time])
     val createdAt: LocalDateTime,
-    
+
     @Field(type = FieldType.Nested)
     val skus: List<BinPackSkuDocument>,
-    
+
     @Field(type = FieldType.Nested)
     val assignments: List<AssignmentDetail>
 ) {
     companion object {
         fun from(
-            result: RecommendResult, 
+            result: RecommendResult,
             skus: List<BinPackSkuDocument>,
             scoreDescription: String,
             scoreCoordinates: List<ScoreCoordinate>,
@@ -66,7 +67,7 @@ data class BinPackRecommendResult(
 data class BinPackSkuDocument(
     @Field(type = FieldType.Long)
     val skuId: Long,
-    
+
     @Field(type = FieldType.Integer)
     val quantity: Int
 )
